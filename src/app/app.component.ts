ca5d90bed08a15565311dc2fec61eff06dec010c
David Lai <laidav@gmail.com>
2025-01-09 23:10:47 -0500

    add comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,35 @@
+/**
+ * SHI WU CHINESE DRINKING GAME (0-5-10-15-20):
+ *
+ * This is a traditional Chinese Drinking game where the loser has to drink!
+ *
+ * But I think we can skipp the drinking and just keep score to be safe.
+ *
+ * HOW TO PLAY:
+ * The outcome of each round can be 0, 5, 10, 15 or 20.
+ *
+ * Each of the two players at the same time decides either to keep their hands closed,
+ * or open 1 hand or two.
+ *
+ * The sum of all the figures opened up is the outcome.
+ *
+ * Each player tries to guess the outcome once before the hand opens.
+ *
+ * If one player is able to guess correctly exclusively they win the round - otherwise its a tie.
+ *
+ * Here is an example of how it would be played in real life: https://www.youtube.com/shorts/JqGABVdmS6g
+ *
+ * In this game you will play against the CPU - GOOD LUCK!
+ *
+ * STEP 1: Pick a target of what you guess the outcome to be.
+ * STEP 2: You then have 3 seconds to decide how many hands to show
+ * STEP 3: The result for the round will be shown
+ *
+ * REPEAT Steps 1-3 until you or the CPU gets too drunk on all the fun.
+ *
+ * GOOD LUCK!
+ *
+ */
 import { Component, Signal, computed } from '@angular/core';
 import { NgClass } from '@angular/common';
 import { toSignal } from '@angular/core/rxjs-interop';
